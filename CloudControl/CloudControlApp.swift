4392142103575e2ea474387b3ccc0b218ac76e49
Edward Samuels <ed@ultara.cloud>
2026-03-13 08:54:35 -0500

    Initial Commit

---
+++ CloudControl/CloudControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  CloudControlApp.swift
+//  CloudControl
+//
+//  Created by dev on 3/13/26.
+//
+
+import SwiftUI
+
+@main
+struct CloudControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
